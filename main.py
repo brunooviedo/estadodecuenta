51888a25ded090fb49b806ffa51dc18161f236bc
bruno <33145112+brunooviedo@users.noreply.github.com>
2024-06-30 08:48:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -44,11 +44,11 @@ if archivo_excel is not None:
         st.write(f'Monto restante disponible: ${monto_restante:.2f}')
 
         # Crear un DataFrame para el gráfico de barras con los índices adecuados
-        df['Descripcion'] = df.iloc[:, 4]  # Columna E (índice 4)
+        df['Descripcion'] = df.iloc[:, 1]  # Columna B (índice 1)
         df['Color'] = ['blue' if x > 0 else 'red' for x in df['Monto']]
 
         # Añadir hover text personalizado con la columna 4 y la columna 10
-        df['Hover'] = df.apply(lambda row: f'Info Columna 4: {row.iloc[4]}<br>Gasto: ${row.iloc[10]:.2f}', axis=1)
+        df['Hover'] = df.apply(lambda row: f'Fecha: {row.iloc[1]}<br>Gasto: ${row.iloc[10]:.2f}', axis=1)
 
         # Generar gráfico de barras con colores asignados
         fig = px.bar(df, x='Descripcion', y='Monto', title='Gastos por Transacción', color='Color', text='Hover')
@@ -59,6 +59,11 @@ if archivo_excel is not None:
         df_gastos = df[df['Monto'] < 0].copy()
         df_gastos['Porcentaje'] = (df_gastos['Monto'] / suma_gastos) * 100
 
+        # Generar gráfico de pastel
+        fig_pie = px.pie(df_gastos, values='Monto', names='Descripcion', title='Distribución de Gastos',
+                         hover_data=['Porcentaje'], labels={'Monto': 'Monto (CLP)'})
+        fig_pie.update_traces(textinfo='percent+label', hovertemplate='Gasto: %{value:.2f}<br>Porcentaje: %{percent:.2%}')
+        st.plotly_chart(fig_pie)
 
     except Exception as e:
         st.error(f'Ocurrió un error al procesar el archivo: {e}')
